82e9e0a54cc233b326d8f8b71a5ac116a278d5a9
AlbanPuytison <alban@hh-dev.com>
2022-11-16 12:52:39 +0100

    Add Plugin Entry | Add Setup Flat Model | Add documentation

---
+++ HHDev.HHDM.DesktopPlugin/PluginEntry.cs
@@ -0,0 +1,34 @@
+﻿using HHDev.DataManagement.Client.Wpf.PluginFramework.Interfaces;
+using System;
+using System.Collections.Generic;
+using HHDev.Core.NETFramework.Models;
+using HHDev.Core.WPF.Layout;
+using HHDev.DataManagement.Client.Wpf.Layout;
+using HHDev.DataManagement.Client.Wpf.Views;
+using HHDev.DataManagement.Client.Wpf.PluginFramework.CustomizationConfigs;
+using HHDev.DataManagement.Client.Core.Models.FlatModels;
+using HHDev.HHDM.DesktopPlugin.Setup;
+
+namespace AndrettiFE.HHDM.DesktopPlugin
+{
+    public class PluginEntry :
+        IHhDataManagementPlugin,
+        ICustomFlatModelProviderPlugin
+    {
+        #region IHhDataManagementPlugin
+        public string Name => "HHDev Plugin";//plugin Name
+        public string AutoUpdateKey => "HHDM-HHDev";// key used to get auto update in HH DM
+        public Guid PluginId => Guid.Parse("{06B9280A-2343-4A7A-9F2E-DB3C82467DC1}"); // Needs to be a unique Guid
+        public Guid[] AccountIds => new Guid[] { Guid.Parse("F2A94AB9-3E13-4FC5-9DBC-E3A88D78E17A") }; // Account Id acessible in HH DM from File => Option
+        #endregion
+
+        #region ICustomFlatModelProviderPlugin
+        public List<FlatModelInitializationDefinition> FlatModelDefinitions => new List<FlatModelInitializationDefinition>()
+        {
+            new FlatModelInitializationDefinition(eFlatModelTypes.Setup, "60d5a9b2-122a-42c2-a093-f9add1ec2bb4", typeof(HHDevRunSheetFlatModel))// Guid of the setup definition(can be found in Admin => Definition)
+        };
+
+        #endregion
+
+    }
+}
---
+++ HHDev.HHDM.DesktopPlugin/Setup/HHDevRunSheetFlatModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HHDev.DataManagement.Client.Core.Models.FlatModels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHDev.HHDM.DesktopPlugin.Setup
+{
+    public class HHDevRunSheetFlatModel : SetupFlatModel
+    {
+        public HHDevRunSheetFlatModel(SetupFlatModelInitializationObject initializationObject, TypeWrapperInitializationContainerWithParts typeWrapperInitializationContainer) : base(initializationObject, typeWrapperInitializationContainer)
+        {
+            base.RegisterParameterAndFunction( // HH DM function with null checks. UpdateRakeCalculation will be called when one of the input parameters changed and if all are non-null
+                new string[] { "RideHeightFL", "RideHeightFR", "RideHeightRL", "RideHeightRR", "Chassis" },//input parameters
+                UpdateRakeCalculation, //Function called
+                new string[] { nameof(RakeAngle) });// parameter updated
+            this.PropertyChanged += HHDevRunSheetFlatModel_PropertyChanged;
+            this.Maths.PropertyChanged += Maths_PropertyChanged;
+        }
+
+        private void Maths_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) //called when a Math parameter from the setup definition changed
+        {
+            if (e.PropertyName == "NameOfSetupMathsParameter")
+            {
+                //call function : Important to add null checks
+            }
+        }
+
+        private void HHDevRunSheetFlatModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) // call when one parameter from the setup definition has changed (parameter or part parameter)
+        {
+            if (e.PropertyName == "NameOfSetupParameter")
+            {
+                //call function : Important to add null checks
+            }
+        }
+
+        private void UpdateRakeCalculation()
+        {
+            var rhFL = Doubles.GetPropertyValue("RideHeightFL");
+            var rhFR = Doubles.GetPropertyValue("RideHeightFR");
+            var rhRL = Doubles.GetPropertyValue("RideHeightRL");
+            var rhRR = Doubles.GetPropertyValue("RideHeightRR");
+            var chassis = Parts.GetPropertyValue("Chassis");
+
+            var rhF = (rhFL + rhFR) / 2;
+            var rhR = (rhRL + rhRR) / 2;
+
+            var wb = chassis.Doubles.GetPropertyValue("Wheelbase");
+            if (!wb.HasValue)
+            {
+                RakeAngle = null;
+                return;
+            }
+            RakeAngle =Math.Asin((double)((rhR - rhF) / wb))*180/Math.PI;
+        }
+
+        public double? RakeAngle { get; set; }
+    }
+}
